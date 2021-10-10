486ecc2a13119e7ae2fe4e5276e8bdb891dedd9a
Christopher Webb <chishiki@gmail.com>
2021-10-10 17:36:04 +0900

    stubbed out a few javascript functions for the product admin interface

--- assets/js/product.js
+++ assets/js/product.js
@@ -1,13 +1,109 @@
 
 var productScript = function() {
 
+	const path = window.location.pathname.split('/');
+	var lang = 'en';
+	var langPrefix = '';
+
+	path.pop();
+	path.shift();
+	if (path[0] == 'ja') {
+		path.shift();
+		lang = 'ja';
+		langPrefix = '/ja';
+	}
+
+
+
+	/* START ADMIN PRODUCT FEATURE MANAGER */
+
+	var setProductFeatureDisplayOrder = function() {
+		var displayOrder = [];
+		$('.admin-product-feature-list-item').each(function(i){
+			displayOrder[i] = $(this).data("product-feature-id");
+		});
+		var settings = {
+			url: "/api/product/update-product-feature-display-order/",
+			method: "post",
+			data: { displayOrder : displayOrder },
+			dataType: "json"
+		};
+		$.ajax(settings);
+	}
+
+	$("#admin_product_feature_list").sortable({
+		handle: ".drag-handle",
+		update: function() { setProductFeatureDisplayOrder(); }
+	});
+
+	var deleteProductFeature = function(productFeatureID) {
+		var settings = {
+			url: "/api/product/delete-product-feature/",
+			method: "post",
+			data: { productFeatureID : productFeatureID },
+			dataType: "json"
+		};
+		$.ajax(settings);
+	}
+
+	$(document).on('click', '.delete-product-feature', function(){
+		var productFeatureID = $(this).parent().data("product-feature-id");
+		$(this).closest('li').remove();
+		deleteProductFeature(productFeatureID);
+	});
+
+	/* END ADMIN PRODUCT FEATURE MANAGER */
+
+
+
+	/* START ADMIN PRODUCT SPECIFICATION MANAGER */
+
+	var setProductSpecificationDisplayOrder = function() {
+		var displayOrder = [];
+		$('.admin-product-specification-list-item').each(function(i){
+			displayOrder[i] = $(this).data("product-specification-id");
+		});
+		var settings = {
+			url: "/api/product/update-product-specification-display-order/",
+			method: "post",
+			data: { displayOrder : displayOrder },
+			dataType: "json"
+		};
+		$.ajax(settings);
+	}
+
+	$("#admin_product_specification_list").sortable({
+		handle: ".drag-handle",
+		update: function() { setProductSpecificationDisplayOrder(); }
+	});
+
+	var deleteProductSpecification = function(productSpecificationID) {
+		var settings = {
+			url: "/api/product/delete-product-specification/",
+			method: "post",
+			data: { productSpecificationID : productSpecificationID },
+			dataType: "json"
+		};
+		$.ajax(settings);
+	}
+
+	$(document).on('click', '.delete-product-specification', function(){
+		var productSpecificationID = $(this).parent().data("product-specification-id");
+		$(this).closest('li').remove();
+		deleteProductSpecification(productSpecificationID);
+	});
+
+	/* END ADMIN PRODUCT SPECIFICATION MANAGER */
+
+
+
 	$(document).on('keypress keyup keydown', '.product-list-autocomplete', function() {
 
 		$('.product-list-autocomplete').autocomplete({
 
 			source: function (request, response) {
 				var settings = {
-					url: "/api/order/product/search/",
+					url: "/api/product/search/",
 					method: "post",
 					data: {'productSearchString':request.term},
 					dataType: "json",
